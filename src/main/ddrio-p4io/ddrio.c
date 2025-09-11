d65417eb4c7a4fa2a45f6774e451aedd6e82c3cc
din <dinsfire64@gmail.com>
2025-09-10 19:26:18 -0500

    tpyo

--- src/main/ddrio-p4io/ddrio.c
+++ src/main/ddrio-p4io/ddrio.c
@@ -137,7 +137,7 @@ bool ddr_io_init(
     }
 
     for (int i = 0; i < nodes; i++) {
-        if (!aciodrv_mdxf_init(mdxf_device, i, false)) {
+        if (!aciodrv_mdxf_init(mdxf_device, i)) {
             log_warning("Opening mdxf device %d failed", i);
             return false;
         }
